84bacadbf0861983d19cb20784df52fca338b9b5
nikita <iladimerov@gmail.com>
2026-05-03 17:45:50 +0300

    added url

--- test_node.js
+++ test_node.js
@@ -1,11 +1,17 @@
-const http = require('http');
+const http = require('http'); // this use when need. func(){http = require('http')}
 const fs = require('fs');
 
 let server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html; charset=utf-8' }); //text/html
+  res.writeHead(200, { 'content-type': 'text/html; charset=utf-8' }); //text/html. WHAT IS THAT
 
-  const stream = fs.createReadStream('./First_project/index.html');
-  stream.pipe(res);
+  if (req.url === '/') {
+    // also learn url and i maked url in the index on the form
+    fs.createReadStream('./index.html').pipe(res); //learn stream
+  } else if (req.url === '/about') {
+    fs.createReadStream('./registration.html').pipe(res);
+  } else {
+    //here "ВЫ ПЕРЕШЛИ НА НЕПРАВИЛЬНУЮ СТРАНИЦУ"
+  }
 });
 
 const PORT = 8080;
