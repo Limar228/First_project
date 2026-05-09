9a6c06fd13f59ec899a487b7a638a1fb60691c1f
nikita <iladimerov@gmail.com>
2026-05-09 13:41:17 +0300

    added env it the script

--- test_node.js
+++ test_node.js
@@ -1,5 +1,6 @@
 const http = require('http'); // this use when need. func(){http = require('http')}
 const fs = require('fs');
+require('dotenv').config();
 
 let server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html; charset=utf-8' }); //text/html. WHAT IS THAT
@@ -7,8 +8,9 @@ let server = http.createServer((req, res) => {
   res.end('dasd');
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3001;
 const HOST = '127.0.0.1';
+console.log(process.env.PORT);
 
 server.listen(PORT, HOST, () => {
   console.log(`Сервер запущен: http://${HOST}:${PORT}`);
