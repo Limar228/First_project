48f71a894e33bfef55d407581ad79cd8ca65d1a7
nikita <iladimerov@gmail.com>
2026-05-11 19:46:10 +0300

    Added .json and remake server for express

--- test_node.js
+++ test_node.js
@@ -1,17 +1,62 @@
-const http = require('http'); // this use when need. func(){http = require('http')}
-const fs = require('fs');
-require('dotenv').config();
+const express = require('express');
+const fs = require('fs/promises');
 
-let server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html; charset=utf-8' }); //text/html. WHAT IS THAT
+const app = express();
 
-  res.end('dasd');
+app.use(express.json());
+
+async function getUsers() {
+  const data = await fs.readFile('users.json', 'utf-8');
+  return JSON.parse(data);
+}
+
+async function writeUser(param) {
+  const data = await fs.writeFile(
+    'users.json',
+    JSON.stringify(param, null, 2),
+    'utf-8',
+  );
+}
+
+app.get('/users', async (req, res) => {
+  const data = await getUsers();
+  res.status(200).json(data);
 });
 
-const PORT = process.env.PORT || 3001;
-const HOST = '127.0.0.1';
-console.log(process.env.PORT);
+app.post('/users', async (req, res) => {
+  const newUser = req.body;
+  const data = await getUsers();
+  const exists = data.some((user) => user.id === newUser.id); //перебор массива говно
 
-server.listen(PORT, HOST, () => {
-  console.log(`Сервер запущен: http://${HOST}:${PORT}`);
+  if (!exists) {
+    if (typeof newUser.id === 'number' && typeof newUser.name === 'string') {
+      data.push(newUser);
+
+      await writeUser(data);
+      return res.status(201).json({
+        message: 'Created',
+        user: newUser,
+      });
+    }
+  }
+  return res.status(400).json({
+    message: 'Вы ввели не правильный запрос',
+  });
+});
+
+app.delete('/users/:id', async (req, res) => {
+  const id = Number(req.params.id);
+
+  let data = await getUsers();
+  data = data.filter((user) => user.id !== id);
+  await writeUser(data);
+  res.status(200).json({
+    message: 'Пользователь удалён',
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
