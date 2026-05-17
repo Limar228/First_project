85343fbcb340c471f2129a1ae35fac402bcef949
nikita <iladimerov@gmail.com>
2026-05-17 13:22:49 +0300

    a litle bit maked

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const { log } = require("console");
-const express = require("express");
-const fs = require("fs/promises");
-const path = require("path");
-const { isTypedArray } = require("util/types");
+const { log, error } = require('console');
+const express = require('express');
+const fs = require('fs/promises');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 
 async function getTasks() {
-  const data = await fs.readFile("tasks.json", "utf-8");
+  const data = await fs.readFile('tasks.json', 'utf-8');
   return JSON.parse(data);
 }
 
@@ -21,34 +20,34 @@ async function writeTasks(param) {
   const getTask = await getTasks();
   getTask.push(param);
   const data = await fs.writeFile(
-    "tasks.json",
+    'tasks.json',
     JSON.stringify(getTask, null, 2),
-    "utf-8",
+    'utf-8',
   );
 }
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
 
-app.get("/reg", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "registration.html"));
+app.get('/reg', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages', 'registration.html'));
 });
 
-app.post("/tasks", async (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = req.body;
   console.log(task);
 
   await writeTasks(task);
-  return res.sendFile(path.join(__dirname, "pages", "index.html"));
+  return res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
 
-app.get("/tasks", async (req, res) => {
+app.get('/tasks', async (req, res) => {
   const data = await getTasks();
-  res.sendFile(path.join(__dirname, "pages", "tasks.html"));
+  res.sendFile(path.join(__dirname, 'pages', 'tasks.html'));
 });
 
-app.get("/api/tasks", async (req, res) => {
+app.get('/api/tasks', async (req, res) => {
   const data = await getTasks();
   res.json(data);
 });
